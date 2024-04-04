67a85395f3a6afffaf3540dd5b16a5e7edb72e8a
enbraining <me@xolving.com>
2024-04-04 22:20:13 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install lazygit
+brew install --cask warp
+brew install --cask raycast
+brew install --cask visual-studio-code
