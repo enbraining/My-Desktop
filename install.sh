3cb15578c73ddb887977946e614f3bfa6c2dae81
enbraining <me@xolving.com>
2024-04-05 00:15:52 +0900

    btop

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install lazygit
+brew install btop
 brew install --cask warp
 brew install --cask raycast
 brew install --cask visual-studio-code
